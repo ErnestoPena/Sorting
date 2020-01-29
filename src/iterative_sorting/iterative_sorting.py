a66f0343745b61ec397157b5c00d671f12370dda
Ernesto Pena <ernestopenad@gmail.com>
2020-01-29 16:49:35 -0600

    STRETCH: implement the Count Sort function. Completed

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -32,8 +32,47 @@ def bubble_sort( arr ):
 
 
 # STRETCH: implement the Count Sort function below
+
+
+
+
 def count_sort( arr, maximum=-1 ):
+    #Edge Case
+    if arr == []:
+        new_arr = []
+        return new_arr
 
-    return arr
+    #Get the Maximum and Minimum
+    minimum = min(arr)
+    maximum = max(arr)
 
-bubble_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])    
+    #Creating new array to avoid modifying the function argument
+    new_arr = []
+
+    if minimum > -1 and maximum > -1:
+
+        #Create Array for placeholding the counts. Array of zero values
+        list_placeholder = [0] * (maximum+1)
+        for i in range(0, len(arr)):
+            list_placeholder[arr[i]] = arr.count(arr[i])
+        '''Filtering to reduce next iteration to the minimum.
+           You can have a range going from 1 to 1,000,000 but with a lot of empty values in between  
+           Eliminating all zero values from list_placeholder.
+           Filtered array structure [[value , quantity] , [value, quantity] , [value...., quantity...]]  
+           Value is list_placeholder index and quantity comes from list_placeholder value   
+        '''
+        filtered_arr = [list((item, list_placeholder[item])) for item in range(len(list_placeholder)) if list_placeholder[item] != 0]
+        #The beef of the problem. Ugh.. looping again...
+        build_single_component = []
+        for i in range(len(filtered_arr)):
+            build_single_component = [filtered_arr[i][0]]*filtered_arr[i][1]
+            new_arr.extend(build_single_component)  
+    else:
+        return "Error, negative numbers not allowed in Count Sort"          
+    return new_arr
+
+
+
+
+#count_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])    
+
