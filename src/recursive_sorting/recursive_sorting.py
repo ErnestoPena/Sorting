426fbdd0e841c5f2176a272c8446e42aa4e22be7
Ernesto Pena <ernestopenad@gmail.com>
2020-01-30 16:15:59 -0600

    This problems has hairs

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,27 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
+# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
+    print(arrA , arrB)
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
-    # TO-DO
+    print("I am here", merged_arr)
     
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
-    # TO-DO
+    #First two Edge cases, when arr is empty and array length iquals one
+    if len(arr) == 0:
+        return []
+    elif len(arr) == 1:
+        return arr
+    else:
+        splitted_list = [[item] for item in arr]
+        initial_call = merge(splitted_list[0] , splitted_list[1])
+        for i in range(2 , len(arr) +1):
+            print(i)
 
-    return arr
+        return arr
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -31,3 +41,6 @@ def merge_sort_in_place(arr, l, r):
 def timsort( arr ):
 
     return arr
+
+
+merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7])
